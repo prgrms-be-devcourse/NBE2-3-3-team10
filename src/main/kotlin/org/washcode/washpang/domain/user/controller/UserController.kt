c2e6155b319c0fdfd359617ff952994060068eb9
HYEONSIK <ks06891@naver.com>
2025-01-23 14:14:49 +0900

    [Feat] Global Exception & Custom Response #2
    - Global Exception 제작
    - Custom Response 제작

--- src/main/kotlin/org/washcode/washpang/domain/user/controller/UserController.kt
+++ src/main/kotlin/org/washcode/washpang/domain/user/controller/UserController.kt
@@ -3,12 +3,10 @@ package org.washcode.washpang.domain.user.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
-import jakarta.validation.Valid
-import lombok.RequiredArgsConstructor
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.washcode.washpang.domain.user.service.UserService
+import org.washcode.washpang.global.exception.ResponseResult
 
 @RestController
 @Tag(name = "User API", description = "유저 인증 관련 API")
@@ -17,7 +15,5 @@ class UserController (private val userService: UserService) {
 
     @PostMapping("/check-login")
     @Operation(summary = "로그인 체크", description = "로그인 체크 API 입니다.")
-    fun checkLogin(request: HttpServletRequest): ResponseEntity<*>{
-        return userService.checkLogin(request);
-    }
+    fun checkLogin(): ResponseResult { return userService.checkLogin() }
 }

3d032995ed1ccdf5aa712b0c8f77e0ef190b59b9
leejc <ljch1104@gmail.com>
2025-01-31 09:08:59 +0900

    [refactor] 공통에러 수정 #25

--- src/main/kotlin/org/washcode/washpang/global/exception/ErrorCode.kt
+++ src/main/kotlin/org/washcode/washpang/global/exception/ErrorCode.kt
@@ -28,7 +28,7 @@ enum class ErrorCode (
 
     // Auth
     TOKEN_NOT_EXPIRED(HttpStatus.OK, "토큰 정상"),
-    TOKEN_EXPIRED(HttpStatus.OK, "토큰 만료됨"),
+    TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "토큰 만료됨"),
 
     // LaundryShop
     FAIL_TO_FIND_LAUNDRYSHOP(HttpStatus.NOT_FOUND, "존재하지 않는 세탁소입니다"),
